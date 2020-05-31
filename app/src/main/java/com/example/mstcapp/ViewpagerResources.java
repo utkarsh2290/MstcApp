560d59f1ed7124bef7153405a4545b741d0bb6ac
Prajesh Kumar G <prajeshofficial007@gmail.com>
2020-06-01 01:20:55 +0530

    commit - 3

--- app/src/main/java/com/example/mstcapp/ViewpagerResources.java
+++ app/src/main/java/com/example/mstcapp/ViewpagerResources.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 public class ViewpagerResources extends FragmentStatePagerAdapter {
 
+    //comments
     private final List<Fragment> fragmentList=new ArrayList<>();
     private final List<String> listTitles=new ArrayList<>();
     public ViewpagerResources(@NonNull FragmentManager fm) {
